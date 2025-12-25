1810ee1511bb3314ce6bcfda81a652ce8f8a7523
Peter Wong <luiges90@gmail.com>
2025-12-25 21:26:45 +0800

    Add Content

---
+++ setup.bat
@@ -0,0 +1,2 @@
+copy Content ZHSan/ZHSan.Core/Content
+copy Content ZHSan/ZHSan.DesktopGL/bin/Debug/net9.0/Content
---
+++ setup.sh
@@ -0,0 +1,2 @@
+cp -r Content ZHSan/ZHSan.Core 
+cp -r Content ZHSan/ZHSan.DesktopGL/bin/Debug/net9.0
